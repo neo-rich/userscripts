060dfdaf04e89d4631c63d66f3f7bb33cb3a5b3f
neo-rich <86172372+neo-rich@users.noreply.github.com>
2022-06-26 13:02:47 +0200

    Update quickmenu.user.js
    Change some styling
    Add shop number prefill
    Add enter event on shop input
    Add timeout removal functionality

--- quickmenu.user.js
+++ quickmenu.user.js
@@ -34,6 +34,7 @@
             text-align: left;
             z-index: 10000;
             font-size: 1rem;
+            box-shadow: 0 2px 6px 0 rgba(0, 0, 0, .25), 0 3px 10px 7px rgba(0, 0, 0, 0.08);
         }
         
         .gm-menu ul {
@@ -42,55 +43,104 @@
             line-height: 1.4;
             list-style-type: none;
         }
-
         .gm-menu a {
             color: black;
             font-weight: 600;
             text-decoration: none;
         }
-
         .gm-menu #gm-shopnum {
             width: 6em;
         }
-
         .gm-menu.legacy {
             left: 10px;
         }
-
-        .gm-menu-close {
+        
+        .top-right {
             position: absolute;
             right: 2px;
             top: 0;
+        }
+        
+        .btn {
             cursor: pointer;
         }
+        .btn:hover {
+            color: gray;
+        }
+        .btn:active {
+            color: red;
+        }
+        
+
     `)
   
     const menu = document.createElement("div");
   
     const isLegacyPage = () => { return !document.querySelector(".nav-top__2020"); }
     
+    let currentShopID = 1;
+    const searchParams = new URLSearchParams(window.location.search);
+    if (searchParams.get("type") === "shop") {
+        currentShopID = parseInt(searchParams.get("obj_type"), 10);
+    }
+    
     menu.innerHTML = `
       <div class="gm-menu${isLegacyPage() ? ' legacy' : ''}">
-        <span class="gm-menu-close">x</span>
+        <div class="top-right">
+          ${ window.neorichTO !== undefined ? `<span class="gm-menu-timeout-button btn">clearTO</span>` : "" }
+          <span class="gm-menu-close btn">x</span>
+        </div>
         <ul>
             <li><a href="/bank.phtml">Bank</a></li>
             <li><a href="/quickref.phtml">QuickRef</a></li>
             <li><a href="/inventory.phtml">Inventory</a>&nbsp;(<a href="/inventory.phtml/x">Old</a>)&nbsp;(<a href="/safetydeposit.phtml">SDB</a>)</li>
             <li><a href="/market.phtml?type=your">Shop Stock</a>&nbsp;(<a href="/market.phtml?type=till">Till</a>)</li>
             <li><a href="/stockmarket.phtml?type=list&bargain=true">Stocks</a>&nbsp;(<a href="/stockmarket.phtml?type=portfolio">Portfolio</a>)</li>
-            <li><a href="/objects.phtml?obj_type=1&type=shop" class="gm-link-shop">Shop #</a><input type="number" id="gm-shopnum" name="gm-shopnum" value="1" min="1"></li>
+            <li><a href="/objects.phtml?obj_type=1&type=shop" class="gm-link-shop">Shop #</a><input type="number" id="gm-shopnum" name="gm-shopnum" value="${currentShopID}" min="1"></li>
         </ul>
       </div>
     `
-
+  
     document.body.appendChild(menu);
 
     document.querySelector("#gm-shopnum").addEventListener("input", function() {
         let shopNum = this.value;
         document.querySelector(".gm-link-shop").href = `/objects.phtml?obj_type=${shopNum}&type=shop`;
     })
+    document.querySelector("#gm-shopnum").addEventListener("keydown", function() {
+        if(event.key === 'Enter') {
+          window.location = document.querySelector(".gm-link-shop").href;
+        }
+    })
   
     document.querySelector(".gm-menu-close").addEventListener("click", () => {
       document.querySelector(".gm-menu").remove();
     })
+  
+    if (window.neorichTO !== undefined) {
+      document.querySelector(".gm-menu-timeout-button").addEventListener("click", () => {
+        document.querySelector(".gm-menu-timeout-button").remove();
+        removeAllTimeouts();
+      })
+    }
+  
+    function removeTimeout(name) {
+        if (window.neorichTO === undefined) {
+          window.neorichTO = {}
+          console.error(`Can't delete timeout "${name}"`)
+        }
+
+        if (window.neorichTO[name]) {
+          clearTimeout(window.neorichTO[name]);
+          console.info(`Removed timeout "${name}".`)
+        } else {
+          console.error(`Can't find timeout "${name}", not deleted.`)
+        }
+    }
+  
+    function removeAllTimeouts() {
+      for (let to of Object.keys(window.neorichTO)) {
+        removeTimeout(to);
+      }
+    }
 })()
